7c52dfb630de9bfbf72a9767eec83d2c751b5b70
KetanAgrawal2002 <98745010+KetanAgrawal2002@users.noreply.github.com>
2023-10-01 08:49:03 +0530

    Add files via upload

--- input.py
+++ input.py
@@ -25,7 +25,7 @@ def addData(data,title):
             creds.refresh(Request())
         else:
             flow = InstalledAppFlow.from_client_secrets_file(
-                'credentials.json', SCOPES)
+                'webcred.json', SCOPES)
             creds = flow.run_local_server(port=0)
         # Save the credentials for the next run
         with open('token.json', 'w') as token:
